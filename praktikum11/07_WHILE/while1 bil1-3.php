77df442c42777dd03f09ba0b6c34098b3ba3f69a
Hera <herah0387@gmail.com>
2025-11-19 19:18:48 +0800

    Add files via upload

---
+++ praktikum11/07_WHILE/while1 bil1-3.php
@@ -0,0 +1,19 @@
+<?php
+$bil = 1;
+while ($bil <= 3) {
+    echo $bil . "<br />";
+    $bil++;
+}
+?>
+
+
+while2 rand.php
+
+<?php
+$bil = rand(0, 10);
+while ($bil != 0) {
+    echo $bil . "<br />";
+    $bil = rand(0, 10);
+}
+?>
+
---
+++ praktikum11/07_WHILE/while4 x10.php
@@ -0,0 +1,21 @@
+<?php
+$bil = 5;
+while ($bil <= 100) {
+    if ($bil % 10 == 0) echo $bil . "<br />";
+    $bil++;
+}
+?>
+
+while5 bilbulat.php
+
+<?php
+$jumlah = 0;
+$bil = 2;
+while ($bil <= 50) {
+    $jumlah = $jumlah + $bil;
+    $bil++;
+}
+
+echo "Hasilnya adalah " . $jumlah;
+?>
+
